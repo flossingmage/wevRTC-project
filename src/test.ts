64b0c03aa17bdb404f1ba44e81586f8fce8409b1
flossingmage <120289005+flossingmage@users.noreply.github.com>
2026-07-08 22:05:01 -0400

    now can run on local host

---
+++ src/test.ts
@@ -0,0 +1,17 @@
+import { FastPeerConnection, type SignalServer } from "./netaware";
+
+export const connect = (isHost: boolean) => {
+  console.log("Connecting as", isHost ? "host" : "client");
+  const signal_server: SignalServer = {
+    makes_first_move: isHost,
+    send_signal_state: async (state: string) => {
+      console.log("Sending signal state:", state);
+    },
+    error_handler: async () => {
+      console.error("Fatal error occurred");
+    },
+  };
+  const timeout_ms = 1000;
+
+  const connection = new FastPeerConnection(signal_server, timeout_ms);
+};
